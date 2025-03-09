d0b92594f999b1ead0e4c1fdc5db80e55cbd5d7b
Raissa Yrina <raissayrina@gmail.com>
2025-03-09 10:23:13 -0300

    Incluindo conteúdo

--- aula_03.py
+++ aula_03.py
@@ -1,1 +1,10 @@
 #aula03_bootcamp
+num = int(input('Insira um número inteiro: '))
+
+if num < 0:
+    print('É menor do que zero')
+
+elif num < 2:
+    print('É menor do que dois')
+else:
+    print('More')
