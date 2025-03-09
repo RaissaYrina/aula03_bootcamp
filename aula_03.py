9910db74006e097d52218e35542aeacdefbc198b
Raissa Yrina <raissayrina@gmail.com>
2025-03-09 10:50:45 -0300

    Incluindo detalhamento sobre FOR

--- aula_03.py
+++ aula_03.py
@@ -1,4 +1,6 @@
 #aula03_bootcamp
+#if
+'''
 num = int(input('Insira um número inteiro: '))
 
 if num < 0:
@@ -8,3 +10,10 @@ elif num < 2:
     print('É menor do que dois')
 else:
     print('More')
+'''
+
+#for
+lista_de_alunos = ['rafael','fabio','luciano']
+
+for aluno in lista_de_alunos:
+    print (aluno)
